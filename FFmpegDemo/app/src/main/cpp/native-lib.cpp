1ba54dfae018f7ac8752ce6d11dc1e659ea07a99
Chen Lin <chelin@microsoft.com>
2022-02-09 15:30:48 +0800

    Update CMakeLists.txt, done for integrating FFmpeg

--- FFmpegDemo/app/src/main/cpp/native-lib.cpp
+++ FFmpegDemo/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,13 @@
 #include <jni.h>
+extern "C" {
+  #include "libavcodec/avcodec.h"
+}
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_chadlin_ffmpegdemo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = avcodec_configuration();
     return env->NewStringUTF(hello.c_str());
 }
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Base on the project https://github.com/Javernaut/ffmpeg-android-maker.
 Current FFmpeg source codes: 4.4.1
 
 Develop branch: dev
+
+For CMakeLists.txt, refer https://proandroiddev.com/a-story-about-ffmpeg-in-android-part-ii-integration-55fb217251f0 for integration into Android
